dfbe6c9d1861d5bff07605711a96b6922a278f4a
kojima-takumi <106937750+kojima-takumi@users.noreply.github.com>
2023-09-29 13:30:54 +0900

    add test code

--- sample/test/sample_web/controllers/page_controller_test.exs
+++ sample/test/sample_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule SampleWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Team-K Framework"
   end
 end
